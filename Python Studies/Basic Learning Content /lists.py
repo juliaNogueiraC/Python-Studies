f23dc300c5b1f17f6d56b06f683fcd4c923168d3
Júlia Nogueira <69528739+juliaNogueiraC@users.noreply.github.com>
2021-12-22 23:23:57 +0000

    new changes

--- Python Studies/Basic Learning Content /lists.py
+++ Python Studies/Basic Learning Content /lists.py
@@ -63,7 +63,7 @@ fruits.append(newfruit)
 for x in range(len(fruits)):
  print(fruits[x])
 
- # without using loop
+# without using loop
   
 a = [1, 2, 3, 4, 5]
   
@@ -109,3 +109,195 @@ print(' '.join(map(str, a)))
 print"in new line"
 print('\n'.join(map(str, a)))
 
+#----------------------------------------------
+#Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+#----------------------------------------------
+
+#Change the second value by replacing it with two new values:
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+#----------------------------------------------
+
+#Change the second and third value by replacing it with one value:
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+#----------------------------------------------
+#Insert "watermelon" as the third item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+#----------------------------------------------
+#Add the elements of tropical to thislist:
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+#----------------------------------------------
+#The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+
+#Example :Add elements of a tuple to a list:
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+#----------------------------------------------
+
+#The remove() method removes the specified item.
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+#The pop() method removes the specified index. if you dont define the index, this method will remove the item
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+#The del keyword also removes the specified index. but it also can delete the entire list
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+
+# the clear method will empty the entire list 
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+#----------------------------------------------
+# LOOP LISTS 
+#Print all items in the list, one by one:
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+#Print all items by referring to their index number:
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+#Print all items, using a while loop to go through all the index numbers
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+
+#A short hand for loop that will print all items in a list:
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+#----------------------------------------------
+# LIST COMPREHENSION 
+#List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list.
+
+#Example:Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+# with comprehension:
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+
+#----------------------------------------------
+#List Alphanumerically
+#Sort the list alphabetically:
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+#Sort the list descending:
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+#----------------------------------------------
+# COPY LISTS
+#You cannot copy a list simply by typing list2 = list1, because: list2 will only be a reference to list1, and changes made in list1 will automatically also be made in list2.
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+#or:
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+#----------------------------------------------
+# Join Lists
+# simple way: 
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+#another way, most interesting, appending all the items from list2 into list1, one by one:
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+#another way.... thsi way add the list2 at the end of list1
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+
+#----------------------------------------------
+#LIST METHODS
+append() #	Adds an element at the end of the list
+
+clear()	#Removes all the elements from the list
+
+copy()	#Returns a copy of the list
+
+count()	#Returns the number of elements with the specified value
+
+extend() #	Add the elements of a list (or any iterable), to the end of the current list
+
+index()#	Returns the index of the first element with the specified value
+
+insert()	#Adds an element at the specified position
+
+pop()	#Removes the element at the specified position
+
+remove()#	Removes the item with the specified value
+
+reverse()#	Reverses the order of the list
+
+sort()	#Sorts the list
--- Python Studies/Basic Learning Content /tuples.py
+++ Python Studies/Basic Learning Content /tuples.py
@@ -26,3 +26,43 @@ fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(fruits[2:5])
 
 #----------------------------------------------
+# tuple as constructor: Using the tuple() method to make a tuple:
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+#----------------------------------------------
+#Check if Item Exists
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+
+#----------------------------------------------
+#Change Tuple Values
+#Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+#But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+#----------------------------------------------
+#Add Items
+#Convert into a list: Just like the workaround for changing a tuple, you can convert it into a list, add your item(s), and convert it back into a tuple.
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+#Add tuple to a tuple. You are allowed to add tuples to tuples, so if you want to add one item, (or many), create a new tuple with the item(s), and add it to the existing tuple:
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+#Note: When creating a tuple with only one item, remember to include a comma after the item, otherwise it will not be identified as a tuple.
+
+#----------------------------------------------
