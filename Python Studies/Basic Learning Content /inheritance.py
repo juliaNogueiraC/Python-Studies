cbc2173142b31ddd10f3a81cebd68dcc00b3ebb6
Júlia Nogueira <69528739+juliaNogueiraC@users.noreply.github.com>
2022-02-10 12:54:11 +0000

    Study about Classes

--- Python Studies/Basic Learning Content /inheritance.py
+++ Python Studies/Basic Learning Content /inheritance.py
@@ -63,10 +63,54 @@ x = Student("Mike", "Olsen")
 x.printname()
 #----------------------------------
 # super function
+# the super function make the child class inherit all the methods and properties from parent 
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
 
+  def printname(self):
+    print(self.firstname, self.lastname)
 
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
 
+x = Student("Mike", "Olsen")
+x.printname()
+
+# so if i use this fuction, it is not necessary to use the parent's name
 
 
 #---------------------------
 # add properties
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
+x = Student("Mike", "Olsen")
+print(x.graduationyear)
+#In the example below, the year 2019 should be a variable, and passed into the Student class when creating student objects. To do so, add another parameter in the __init__() function:
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
+#------------------------------------
+#add methods
+# add the method "Welcome" to the student 
+ def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+x = Student("Mike", "Olsen", 2019)
+x.welcome()
