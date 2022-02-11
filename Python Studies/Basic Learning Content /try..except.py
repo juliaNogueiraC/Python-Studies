b6bc1dcbf7c2c2560152f3e84bd64d3811c038f4
Júlia Nogueira <69528739+juliaNogueiraC@users.noreply.github.com>
2022-02-11 20:15:20 +0000

    exceptions

---
+++ Python Studies/Basic Learning Content /try..except.py
@@ -0,0 +1,7 @@
+ try #block lets you test a block of code for errors.
+
+except #block lets you handle the error.
+
+ else #block lets you execute code when there is no error.
+
+finally #block lets you execute code, regardless of the result of the try- and except blocks.
